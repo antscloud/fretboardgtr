e61dbe71eb4b80b66234c069379020d82e999060
Antoine Gibek <antoine.gibek@ipsl.fr>
2023-02-16 13:39:41 +0100

    fix: Two times the same test

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 
-from fretboardgtr.fretboard import FretBoard
+from fretboardgtr.fretboard import FretBoard, VerticalFretBoard
 from fretboardgtr.notes_creators import ScaleFromName
 
 C_IONIAN_SVG = Path(__file__).parent / "data" / "c_ionian.svg"
@@ -13,7 +13,7 @@ def remove_test_file(path: Path):
         os.remove(str(path))
 
 
-def test_c_major_fretboard():
+def test_c_major_fretboard(remove_test_file):
     fretboard = FretBoard()
     c_major = ScaleFromName(root="C", mode="Ionian").get()
     fretboard.add_notes(scale=c_major)
@@ -25,8 +25,8 @@ def test_c_major_fretboard():
     assert tmp_file.exists()
 
 
-def test_c_major_vertical_fretboard():
-    fretboard = FretBoard()
+def test_c_major_vertical_fretboard(remove_test_file):
+    fretboard = VerticalFretBoard()
     c_major = ScaleFromName(root="C", mode="Ionian").get()
     fretboard.add_notes(scale=c_major)
     os.makedirs(OUTPUTS_ARTIFACT_FOLDER, exist_ok=True)
